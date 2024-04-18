0b4f55e457631f93b219b4d469356d30beb6907d
MauricioHernandez <mauriciohernandez@Mauricios-MacBook-Pro.local>
2024-04-18 10:44:26 -0500

    second role

--- main.tf
+++ main.tf
@@ -26,3 +26,28 @@ resource "aws_iam_role" "test_role" {
     tag-key = "tag-value"
   }
 }
+
+resource "aws_iam_role" "test_role1" {
+  name = "test_role1"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  permissions_boundary =         "arn:aws:iam::123456789012:policy/oxy-secpol-required-permission-boundary"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
